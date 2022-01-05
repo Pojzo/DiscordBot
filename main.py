bc61c94bc230e0a84f5c0a48aae52eeb1ef16568
matuskotheone <matus.gaz@gmail.com>
2022-01-05 20:12:55 +0100

    zedols

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    print(message.content)
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
