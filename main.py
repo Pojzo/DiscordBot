35ca423dfcd297e1e0011244dd67ea1453ce8fad
Pojzo <pojzinko8@gmail.com>
2022-01-13 19:04:06 +0100

    Images

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import discord
 from discord_token import token
+import glob
+import numpy as np
+import random
+import cv2
 
 images_people = ["gazdik", "pojzo", "marek", "david"]
-images = {"gazdik" : }
 
 client = discord.Client()
+
+def get_images(name: str) -> np.array:
+    images = []
+    return [filename for filename in glob.glob(f'images/{name}/*.jpg')]
+
+    image = cv2.imread(filename)
+    images.append(image)
+
+
+    return images
 
 @client.event
 async def on_ready():
@@ -15,18 +28,23 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if not message.startswith("!"):
+    if not message.content.startswith("!"):
         return
-    message = message[1:]
-    split_message = message.split()
+    msg = message.content[1:]
+    split_message = msg.split()
     if len(split_message) == 1:
         message.channel.send("Zly command brasko")
         return
 
     first, second = split_message
+    second = second.lower()
     if first == "image":
-        if tolower(second) in images_people:
-
+        if second in images_people:
+            images = get_images(second)
+            # print(images)
+            image = random.choice(images)
+            print(image)
+            await message.channel.send(file = discord.File(random.choice(images)))
 
 
 client.run(token)
